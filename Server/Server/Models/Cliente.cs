a33d2f1448ea4aa554a14e6122b8ba0b5b8ad3b8
Caique Lima <caique.lima@elotech.com.br>
2020-11-23 05:21:07 -0300

    Script criacao

--- Server/Server/Models/Cliente.cs
+++ Server/Server/Models/Cliente.cs
@@ -11,46 +11,58 @@ namespace Server.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Display(Name = "Id")]
         [Column("Id")]
+        [Required]
         public int Id { get; set; }
 
         [Display(Name = "Nome")]
         [Column("Nome")]
+        [Required]
+        [MaxLength(70)]
         public string Nome { get; set; }
 
         [Display(Name = "DataNascimento")]
         [Column("DataNascimento")]
+        [Required]
         public DateTime DataNascimento { get; set; }
 
         [Display(Name = "Sexo")]
         [Column("Sexo")]
+        [MaxLength(10)]
         public string? Sexo { get; set; }
 
         [Display(Name = "Cep")]
         [Column("Cep")]
+        [MaxLength(8)]
         public string? Cep { get; set; }
 
         [Display(Name = "Endereço")]
         [Column("Endereco")]
+        [MaxLength(70)]
         public string? Endereco { get; set; }
 
         [Display(Name = "Número")]
         [Column("Numero")]
+        [MaxLength(10)]
         public string? Numero { get; set; }
 
         [Display(Name = "Complemento")]
         [Column("Complemento")]
+        [MaxLength(20)]
         public string? Complemento { get; set; }
 
         [Display(Name = "Bairro")]
         [Column("Bairro")]
+        [MaxLength(30)]
         public string? Bairro { get; set; }
 
         [Display(Name = "Estado")]
         [Column("Estado")]
+        [MaxLength(20)]
         public string? Estado { get; set; }
 
         [Display(Name = "Cidade")]
         [Column("Cidade")]
+        [MaxLength(50)]
         public string? Cidade { get; set; }
     }
 }
